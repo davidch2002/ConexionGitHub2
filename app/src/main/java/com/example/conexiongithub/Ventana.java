0ecbf17b1d71ccaa2e0993e02bac974b9c516771
DAVID <david_masna_123@hotmail.com>
2022-08-29 10:41:46 -0500

    Esto es el primer commit de mi aplicación.

---
+++ app/src/main/java/com/example/conexiongithub/Ventana.java
@@ -0,0 +1,14 @@
+package com.example.conexiongithub;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Ventana extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ventana);
+    }
+}
